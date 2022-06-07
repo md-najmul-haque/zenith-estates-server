e2610772883f374e5e11b2a0ca4e4cd7eb20e732
najuml-bd <najmul0612@gmail.com>
2022-06-07 16:22:06 +0600

    review api added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
-// middlewear
+// middlewear   
 app.use(cors());
 app.use(express.json());
 
@@ -12,12 +12,30 @@ app.use(express.json());
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lyxfj.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    console.log('db connected')
-    client.close();
-});
+
+async function run() {
+    try {
+        await client.connect();
+        const apartmentsCollection = client.db("zenith_estates").collection("apartments");
+        const reviewsCollection = client.db("zenith_estates").collection("reviews");
+
+        app.get('/apartments', async (req, res) => {
+            const query = {};
+            const result = await apartmentsCollection.find(query).toArray();
+            res.send(result)
+        })
+
+        app.get('/reviews', async (req, res) => {
+            const query = {};
+            const result = await reviewsCollection.find(query).toArray()
+            res.send(result)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
 
 
 app.get('/', (req, res) => {
